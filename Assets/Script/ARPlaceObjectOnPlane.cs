246ad5dd530e511f60ed00c19d4420f6526a7b3a
Jason XXR <jasonxu906@gmail.com>
2021-02-26 23:49:50 -0800

    Created test scene which places a prefab when player taps the screen
    Scene name: AR Mechanic test scene 1

---
+++ Assets/Script/ARPlaceObjectOnPlane.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARPlaceObjectOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    [SerializeField]
+    GameObject m_ObjectToPlace;
+
+    void Update()
+    {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began) {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits)) {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    Instantiate(m_ObjectToPlace, hitPose.position, hitPose.rotation);
+				}
+			}
+		}
+    }
+}
+
